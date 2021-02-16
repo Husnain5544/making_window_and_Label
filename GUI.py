ffb90f177a71ad4ba57033383479357dd536c495
Husnain5544 <79162537+Husnain5544@users.noreply.github.com>
2021-02-16 22:55:47 +0500

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,14 @@
+# importing GUI library
+from tkinter import *
+
+# making window
+root = Tk()
+
+# making Label
+label = Label(root, text="The Label")
+
+# Show the label
+label.pack()
+
+# for window opening
+root.mainloop()
